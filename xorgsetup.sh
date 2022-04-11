c7b69d9b3d14be4d3f1d74aff14573f0af8a5998
Roberto <rar@kaelsakja>
2022-04-10 21:17:29 -0300

    updated settings for Debian

--- xorgsetup.sh
+++ xorgsetup.sh
@@ -1,4 +1,5 @@
 # dvorak with compose key and caps lock as control
 setxkbmap dvorak -option ctrl:nocaps -option lv3:ralt_switch -option compose:ralt
-# disable middle click on right side of touchpad
-xinput set-button-map "ETPS/2 Elantech Touchpad" 1 1 3 4 5 6 7
+
+# disable bell sound on Debian
+rmmod pcspkr ; echo "blacklist pcspkr" >>/etc/modprobe.d/blacklist.conf
