51272143ce8da01bcf08ea23a5ab7d72d49b2aa3
Roberto <rar@kaelsakja>
2022-04-25 03:59:42 -0300

    keyboard settings for virtual terminal

--- xorgsetup.sh
+++ xorgsetup.sh
@@ -1,5 +1,3 @@
-# dvorak with compose key and caps lock as control
-setxkbmap dvorak -option ctrl:nocaps -option lv3:ralt_switch -option compose:ralt
 
 # disable bell sound on Debian
 rmmod pcspkr ; echo "blacklist pcspkr" >>/etc/modprobe.d/blacklist.conf
